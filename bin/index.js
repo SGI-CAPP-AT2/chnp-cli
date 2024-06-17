75a04888da02a30f22d8c3482bdc6bfb202c5ca6
Big-oh-learn <shubhamingale779@gmail.com>
2024-06-17 12:10:16 +0530

    Added new command cohl

---
+++ bin/index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+const process = require("process");
+const operators = require("./operators");
+const { main } = require("../helpers/mainExecution.js");
+const op = process.argv[2],
+  args = process.argv.reduce((p, c, i) => (i > 2 ? [...p, c] : p), []),
+  wd = process.cwd();
+main({ operators, op, wd, args });
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # Code Highlight n Print Command Line Interface
 
 This tool is cli version for CHNP project which works locally into your computers.
+

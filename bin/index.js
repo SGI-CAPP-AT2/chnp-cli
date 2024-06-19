501f28907b66cecce7e0af21dc7c8a0dbd4b05d6
Big-oh-learn <shubhamingale779@gmail.com>
2024-06-19 20:51:58 +0530

    unnecessary log removed

--- bin/index.js
+++ bin/index.js
@@ -5,5 +5,4 @@ const { main } = require("../helpers/mainExecution.js");
 const op = process.argv[2],
   args = process.argv.reduce((p, c, i) => (i > 2 ? [...p, c] : p), []),
   wd = process.cwd();
-console.log(operators);
 main({ operators, op, wd, args });
